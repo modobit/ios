d84b6ee004648c61129094dbafc65b8c5398ec5d
modo <modobit@users.noreply.github.com>
2021-02-13 12:56:34 -0500

    update

---
+++ Swift Basic/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+var mystring = "Hello this is my string"
+
+
+print("Hello, Swift Apprentice reader!")
+print(mystring)
+
+
+//math functions test
+print (sin(45 * Double.pi / 180))
+print ((2.0).squareRoot() )
+
+//declaring intnumbers in the list
+
+let pi: Double = 3.14159
+
+let number: Int = 10
+var variableNumber: Int = 42
+
+
