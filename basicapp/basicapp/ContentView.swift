d5baed87477f67072072fe648c4916ebb2513954
modo <modobit@users.noreply.github.com>
2021-02-13 20:10:36 -0500

    update code

--- basicapp/basicapp/ContentView.swift
+++ basicapp/basicapp/ContentView.swift
@@ -13,27 +13,50 @@ struct ContentView: View {
     @State var date = Date()
     
     var body: some View {
-        VStack  {
-            Text("Hello Wicar")
-                .font(.largeTitle)
-                .bold()
-                .padding()
+        
+        ScrollView {
             
-            Text("My simple iOS applciation")
-                .padding(.bottom)
-            
-            Text("Version 1.0")
-                .padding(.bottom)
-            
-            Text("\(date)")
-            
-            
-            
+            VStack  {
+                
+                Label("SwiftUI Tutorials", systemImage: "book.fill")
+                Text("Hello Wicar")
+                    .font(.title)
+                    .fontWeight(.black)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Text("My simple iOS applciation")
+                    .padding(.bottom)
+                
+                
+                Text("\(date)")
+                    .foregroundColor(.black)
+                    .font(.system(size: 12))
+                    .padding(20)
+                    .multilineTextAlignment(.center)
+                
+                
+                Image("einstien")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 300.0, height: 400.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Text("Hello World")
+                    )
+                
+                
+                Image(systemName: "paperplane.circle.fill")
+                    .font(.system(size: 70))
+                    .foregroundColor(.red)
+                
+                
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 50, height: 50)
+                
+            }
             
         }
-    
-        
-            
     }
 }
 
